072662519c4586bea4fa47898f2a78abf95aee41
Denis <and1helicopter@mail.ru>
2018-06-08 16:29:43 +0300

    v1.0a0.02
    +Add Form Verification

--- ExMonApplication3/ExMonApplication2/VerificationForm.Designer.cs
+++ ExMonApplication3/ExMonApplication2/VerificationForm.Designer.cs
@@ -28,19 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ChanneldataGridView = new System.Windows.Forms.DataGridView();
+            this.Column_addrOutput = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_bitOutput = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column_addrInput = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_bitInput = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChanneldataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(137, 12);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество итераций:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 38);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Сценарий 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(104, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Сценарий 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(193, 38);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(83, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "Сценарий 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(287, 38);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(50, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.Text = "Свой";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton4_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(286, 12);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Количество:";
+            // 
+            // ChanneldataGridView
+            // 
+            this.ChanneldataGridView.AllowUserToAddRows = false;
+            this.ChanneldataGridView.AllowUserToDeleteRows = false;
+            this.ChanneldataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChanneldataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ChanneldataGridView.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.ChanneldataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ChanneldataGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenHorizontal;
+            this.ChanneldataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.ChanneldataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ChanneldataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_addrOutput,
+            this.Column_bitOutput,
+            this.Column_addrInput,
+            this.Column_bitInput});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ChanneldataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ChanneldataGridView.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.ChanneldataGridView.Location = new System.Drawing.Point(15, 61);
+            this.ChanneldataGridView.Margin = new System.Windows.Forms.Padding(0);
+            this.ChanneldataGridView.Name = "ChanneldataGridView";
+            this.ChanneldataGridView.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.ChanneldataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.ChanneldataGridView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ChanneldataGridView.Size = new System.Drawing.Size(385, 391);
+            this.ChanneldataGridView.TabIndex = 13;
+            // 
+            // Column_addrOutput
+            // 
+            this.Column_addrOutput.HeaderText = "Выход ";
+            this.Column_addrOutput.Name = "Column_addrOutput";
+            this.Column_addrOutput.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column_addrOutput.ToolTipText = "Адрес";
+            // 
+            // Column_bitOutput
+            // 
+            this.Column_bitOutput.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
+            this.Column_bitOutput.HeaderText = "Выход";
+            this.Column_bitOutput.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.Column_bitOutput.Name = "Column_bitOutput";
+            this.Column_bitOutput.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column_bitOutput.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column_bitOutput.ToolTipText = "Бит";
+            // 
+            // Column_addrInput
+            // 
+            this.Column_addrInput.HeaderText = "Вход";
+            this.Column_addrInput.Name = "Column_addrInput";
+            this.Column_addrInput.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column_addrInput.ToolTipText = "Адрес";
+            // 
+            // Column_bitInput
+            // 
+            this.Column_bitInput.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
+            this.Column_bitInput.HeaderText = "Вход";
+            this.Column_bitInput.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.Column_bitInput.Name = "Column_bitInput";
+            this.Column_bitInput.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column_bitInput.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column_bitInput.ToolTipText = "Бит";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(405, 38);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Результат:";
             // 
             // VerificationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 278);
+            this.ClientSize = new System.Drawing.Size(509, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChanneldataGridView);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.MinimumSize = new System.Drawing.Size(525, 500);
             this.Name = "VerificationForm";
             this.Text = "VerificationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ChanneldataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView ChanneldataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_addrOutput;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column_bitOutput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_addrInput;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column_bitInput;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- ExMonApplication3/ExMonApplication2/VerificationForm.cs
+++ ExMonApplication3/ExMonApplication2/VerificationForm.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 using System.Windows.Forms;
 using StandartScreens;
 using UniSerialPort;
@@ -13,9 +9,230 @@ namespace ExMonApplication3
 {
     public partial class VerificationForm : StandartScreenTemplateForm
     {
+        AsynchSerialPort _serialPort;
+        bool _portBusy;
+
+        readonly object[] _bit =
+        {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"
+        };
+
+        private readonly List<Info> _status1 = new List<Info>()
+        {
+            new Info("0x0120", "7", "0x0140", "0"),
+            new Info("0x0120", "6", "0x0140", "1"),
+            new Info("0x0120", "5", "0x0140", "2"),
+            new Info("0x0120", "4", "0x0140", "3"),
+            new Info("0x0120", "3", "0x0140", "4"),
+            new Info("0x0120", "2", "0x0140", "5"),
+            new Info("0x0120", "1", "0x0140", "6"),
+            new Info("0x0120", "0", "0x0140", "7"),
+            new Info("0x0120", "8", "0x0140", "8"),
+            new Info("0x0120", "9", "0x0140", "9"),
+            new Info("0x0120", "10", "0x0140", "10"),
+            new Info("0x0120", "11", "0x0140", "11"),
+            new Info("0x0120", "12", "0x0140", "12"),
+            new Info("0x0120", "13", "0x0140", "13"),
+            new Info("0x0120", "14", "0x0140", "14"),
+            new Info("0x0120", "15", "0x0140", "15")
+        };
+
+        private readonly List<Info> _status2 = new List<Info>()
+        {
+            new Info("0x0124", "7", "0x0144", "0"),
+            new Info("0x0124", "6", "0x0144", "1"),
+            new Info("0x0124", "5", "0x0144", "2"),
+            new Info("0x0124", "4", "0x0144", "3"),
+            new Info("0x0124", "3", "0x0144", "4"),
+            new Info("0x0124", "2", "0x0144", "5"),
+            new Info("0x0124", "1", "0x0144", "6"),
+            new Info("0x0124", "0", "0x0144", "7"),
+            new Info("0x0128", "8", "0x0144", "8"),
+            new Info("0x0128", "9", "0x0144", "9"),
+            new Info("0x0128", "10", "0x0144", "10"),
+            new Info("0x0128", "11", "0x0144", "11"),
+            new Info("0x0128", "12", "0x0144", "12"),
+            new Info("0x0128", "13", "0x0144", "13"),
+            new Info("0x0128", "14", "0x0144", "14"),
+            new Info("0x0128", "15", "0x0144", "15")
+        };
+
+        private readonly List<Info> _status3 = new List<Info>()
+        {
+            new Info("0x012C", "7", "0x0148", "0"),
+            new Info("0x012C", "6", "0x0148", "1"),
+            new Info("0x012C", "5", "0x0148", "2"),
+            new Info("0x012C", "4", "0x0148", "3"),
+            new Info("0x012C", "3", "0x0148", "4"),
+            new Info("0x012C", "2", "0x0148", "5"),
+            new Info("0x012C", "1", "0x0148", "6"),
+            new Info("0x012C", "0", "0x0148", "7"),
+            new Info("0x012C", "8", "0x0148", "8"),
+            new Info("0x012C", "9", "0x0148", "9"),
+            new Info("0x012C", "10", "0x0148", "10"),
+            new Info("0x012C", "11", "0x0148", "11"),
+            new Info("0x012C", "12", "0x0148", "12"),
+            new Info("0x012C", "13", "0x0148", "13"),
+            new Info("0x012C", "14", "0x0148", "14"),
+            new Info("0x012C", "15", "0x0148", "15")
+        };
+
+        private List<Info> _statusCurrent = new List<Info>();
+
         public VerificationForm(AsynchSerialPort AsynchSerialPort)
         {
             InitializeComponent();
+
+            _serialPort = AsynchSerialPort;
+            SetGrid(_status1, false);
         }
+
+        private void SetGrid(List<Info> list, bool editable)
+        {
+            ChanneldataGridView.Rows.Clear();
+            numericUpDown2.Value = 0;
+            _statusCurrent = list;
+
+            ChanneldataGridView.EditMode = editable?DataGridViewEditMode.EditOnKeystrokeOrF2:DataGridViewEditMode.EditProgrammatically;
+            numericUpDown2.Enabled = editable;
+
+            foreach (var itemInfo in list)
+            {
+                ChanneldataGridView.Rows.Add(itemInfo.AddrOutput, itemInfo.BitOutput, itemInfo.AddrInput, itemInfo.BitInput);
+                ChanneldataGridView.Rows[ChanneldataGridView.RowCount - 1].HeaderCell.Value = (ChanneldataGridView.RowCount).ToString();
+                numericUpDown2.Value++;
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                if (ChanneldataGridView.RowCount > numericUpDown2.Value)
+                    ChanneldataGridView.Rows.RemoveAt(ChanneldataGridView.RowCount - 1);
+                else if(ChanneldataGridView.RowCount < numericUpDown2.Value)
+                {
+                    ChanneldataGridView.Rows.Add("0x0000", _bit[0], "0x0000", _bit[0]);
+
+                    var index = ChanneldataGridView.RowCount - 1;
+                    ChanneldataGridView.Rows[index].HeaderCell.Value = (index + 1).ToString();
+                }
+            }
+        }
+
+        private class Info
+        {
+            public string AddrInput { get; }
+            public string BitInput { get; }
+            public string AddrOutput { get; }
+            public string BitOutput { get; }
+
+            public Info(string addrInput, string bitInput, string addrOutput, string bitOutput)
+            {
+                AddrInput = addrInput;
+                BitInput = bitInput;
+                AddrOutput = addrOutput;
+                BitOutput = bitOutput;
+            }
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            SetGrid(_status1, false);
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            SetGrid(_status2, false);
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            SetGrid(_status3, false);
+        }
+
+        private void radioButton4_Click(object sender, EventArgs e)
+        {
+            SetGrid(_statusCurrent, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestListInput.Clear();
+            foreach (var current in _statusCurrent)
+            {
+                if (TestListInput.ContainsKey(int.Parse(current.AddrInput.Substring(2), NumberStyles.HexNumber)))
+                {
+                    TestListInput[int.Parse(current.AddrInput.Substring(2), NumberStyles.HexNumber)].Add(int.Parse(current.BitInput), false);
+                }
+                else
+                {
+                    TestListInput.Add(int.Parse(current.AddrInput.Substring(2), NumberStyles.HexNumber), new Dictionary<int, bool> { { int.Parse(current.BitInput), false } });
+                }
+            }
+        }
+
+        Dictionary<int, Dictionary<int, bool>> TestListInput = new Dictionary<int, Dictionary<int, bool>>();
+
+        
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!_serialPort.IsOpen) { return; }
+            if (!this.Visible) { return; }
+            if (_portBusy) { return; }
+            _portBusy = true;
+            //_serialPort.SetDataRTU((ushort)(startAddr + 1), null, RequestPriority.Normal, maskOR, maskAND);
+            //_serialPort.GetDataRTU((ushort)(startAddr + 3), 1, DataRecieved);
+        }
+
+        private void DataRecieved(bool DataOk, ushort[] ParamRTU)
+        {
+            //    if (InvokeRequired)
+            //    {
+            //        Invoke(new AsynchSerialPort.DataRecievedRTU(DataRecieved), DataOk, ParamRTU);
+            //    }
+            //    else
+            //    {
+
+            //        portBusy = false;
+            //        if (DataOk)
+            //        {
+            //            ushort u = ParamRTU[0];
+            //            if (invert)
+            //            {
+            //                u = (ushort)(u ^ 0xFFFF);
+            //            }
+            //            for (int i = 0; i < 16; i++)
+            //            {
+            //                if (ConvertFuncs.GetBit(u, i))
+            //                {
+            //                    lampLabels[i].BackColor = Color.LightGreen;
+            //                }
+            //                else
+            //                {
+            //                    lampLabels[i].BackColor = Color.White;
+            //                }
+            //            }
+            //        }
+
+            //    }
+        }
+
     }
 }
